0ffb752862910a6a71521745b81b7e9c71d1c9a4
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-18 12:44:47 +0100

    [HeaderInfoBar] style: ingtroducing UI and RWD to the component

--- components/HeaderInfoBar/HeaderInfoBar.component.tsx
+++ components/HeaderInfoBar/HeaderInfoBar.component.tsx
@@ -1,24 +1,67 @@
 import Image from "next/image";
 import s from "./HeaderInfoBar.component.module.scss";
 
+import telephone from "@/assets/icons/telephone.png";
+import mail from "@/assets/icons/mail.png";
+
+import facebook from "@/assets/icons/facebook.png";
+import linkedin from "@/assets/icons/linkedin.png";
+import shop from "@/assets/icons/shop.png";
+
 function HeaderInfoBar() {
   return (
-    <section>
-      <section>
-        <div>
-          <Image src={""} alt="" />
-          <span>928 53 56 58</span>
+    <section className={s.container}>
+      <section className={s.container__contactSection}>
+        <div className={s.container__contactSection__telephoneNumber}>
+          <Image
+            className={s.container__contactSection__telephoneNumber__icon}
+            width={30}
+            height={30}
+            src={telephone}
+            alt=""
+          />
+          <span
+            className={s.container__contactSection__telephoneNumber__content}
+          >
+            928 53 56 58
+          </span>
         </div>
-        <div>
-          <Image src={""} alt="" />
-          <span>928 53 56 58</span>
+        <div className={s.container__contactSection__emailAdress}>
+          <Image
+            className={s.container__contactSection__emailAdress__icon}
+            width={30}
+            height={30}
+            src={mail}
+            alt=""
+          />
+          <span className={s.container__contactSection__emailAdress__content}>
+            infmovilweb@gmail.com
+          </span>
         </div>
       </section>
-      <section>
-        <div>
-          <Image src={""} alt="" />
-          <Image src={""} alt="" />
-          <Image src={""} alt="" />
+      <section className={s.container__iconsSection}>
+        <div className={s.container__iconsSection__wrapper}>
+          <Image
+            className={s.container__iconsSection__wrapper__icon}
+            src={facebook}
+            alt=""
+            width={30}
+            height={30}
+          />
+          <Image
+            className={s.container__iconsSection__wrapper__icon}
+            src={shop}
+            alt=""
+            width={30}
+            height={30}
+          />
+          <Image
+            className={s.container__iconsSection__wrapper__icon}
+            src={linkedin}
+            alt=""
+            width={30}
+            height={30}
+          />
         </div>
       </section>
     </section>
