d4b9752ce8777ad778751e1405e282d39c75ac95
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-18 15:43:34 +0100

    [Header] animation: introduce animation to scroll sticky menu

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ export default function Home() {
     <HeaderInfoBar />
     <Header />
     <HomePage />
+    <HomePage />
+    <HomePage />
     </>
   );
 }
--- components/Header/Header.component.tsx
+++ components/Header/Header.component.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import s from "./Header.component.module.scss";
 import HamburgerLogo from "./HamburgerLogo/HamburgerLogo.component";
 import HamburgerMenu from "./HamburgerMenu/HamburgerMenu";
@@ -6,11 +8,31 @@ import logo from "@/assets/images/logo.jpeg";
 
 import SearchSetion from "./SearchSection/SearchSetion";
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 
 const Header = () => {
+  const header = useRef<HTMLDivElement>(null);
+  const [isSticky, setIsSticky] = useState(false);
+
+  const handleScroll = () => {
+    if (header.current) {
+      if (scrollY > 100) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    addEventListener("scroll", handleScroll);
+    return () => {
+      removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <>
-      <header className={s.header}>
+      <header ref={header} className={` ${isSticky ? s.sticky : s.header}`}>
         <section className={s.header__logoSection}>
           <Image src={logo} alt="logo of company" width={100} height={100} />
         </section>
