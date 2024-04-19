dce8a57e2b9e6d5b07f01c98dc30ab1933c9b23b
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-19 13:35:05 +0100

    [Services] class: introduce BEM class

--- components/Services-component/Services.component.tsx
+++ components/Services-component/Services.component.tsx
@@ -1,33 +1,69 @@
 import Image from "next/image";
 import s from "./Services.component.module.scss";
 
-import arrow from '@/assets/icons/arrow.png';
-import image from '@/assets/images/compouter_repair.jpeg';
+import arrow from "@/assets/icons/arrow.png";
+import image from "@/assets/images/compouter_repair.jpeg";
 
 function Services() {
   return (
-    <section>
-      <section>
-        <div>
-          <span>Sobre Nosotros</span>
-          <h2>Nuestros servicios</h2>
+    <section className={s.container}>
+      <section className={s.container__header}>
+        <div className={s.container__header__wrapper}>
+          <span className={s.container__header__wrapper__category}>
+            Sobre Nosotros
+          </span>
+          <h2 className={s.container__header__wrapper__title}>
+            Nuestros servicios
+          </h2>
         </div>
       </section>
-      <section>
-        <div>
-          <div>
-            <div>
-              <Image src={image} alt="service-image" />
-            </div>
-            <div>
-              <h3>reparación de ordenador</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-            </div>
-            <div>
-              <div>
-                <span>leer más</span>
-                <Image src={arrow} alt="arrow-icon" />
-              </div>
+      <section className={s.container__servicesContainer}>
+        <div className={s.container__servicesContainer__wrapper}>
+          <div
+            className={s.container__servicesContainer__wrapper__imageWrapper}
+          >
+            <Image
+              className={
+                s.container__servicesContainer__wrapper__imageWrapper__img
+              }
+              src={image}
+              alt="service-image"
+            />
+          </div>
+          <div className={s.container__servicesContainer__wrapper__textWrapper}>
+            <h3
+              className={
+                s.container__servicesContainer__wrapper__textWrapper__title
+              }
+            >
+              reparación de ordenador
+            </h3>
+            <p
+              className={
+                s.container__servicesContainer__wrapper__textWrapper__description
+              }
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            </p>
+          </div>
+          <div
+            className={s.container__servicesContainer__wrapper__actionSection}
+          >
+            <div
+              className={
+                s.container__servicesContainer__wrapper__actionWrapper__wrapper
+              }
+            >
+              <span
+                className={
+                  s.container__servicesContainer__wrapper__actionWrapper__wrapper__text
+                }
+              >
+                leer más
+              </span>
+              <Image  className={
+                  s.container__servicesContainer__wrapper__actionWrapper__wrapper__icon
+                } src={arrow} alt="arrow-icon" />
             </div>
           </div>
         </div>
