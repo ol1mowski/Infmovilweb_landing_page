c3c65f3c8b9bd2ca1cd47ea8b6e8c968bccb0677
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-19 13:31:27 +0100

    [Services] sketch: introduce TSX sketch

--- components/Services-component/Services.component.tsx
+++ components/Services-component/Services.component.tsx
@@ -1,9 +1,39 @@
-import s from './Services.component.module.scss';
+import Image from "next/image";
+import s from "./Services.component.module.scss";
+
+import arrow from '@/assets/icons/arrow.png';
+import image from '@/assets/images/compouter_repair.jpeg';
 
 function Services() {
   return (
-    <div>Services</div>
-  )
+    <section>
+      <section>
+        <div>
+          <span>Sobre Nosotros</span>
+          <h2>Nuestros servicios</h2>
+        </div>
+      </section>
+      <section>
+        <div>
+          <div>
+            <div>
+              <Image src={image} alt="service-image" />
+            </div>
+            <div>
+              <h3>reparación de ordenador</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            </div>
+            <div>
+              <div>
+                <span>leer más</span>
+                <Image src={arrow} alt="arrow-icon" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
 }
 
-export default Services
+export default Services;
