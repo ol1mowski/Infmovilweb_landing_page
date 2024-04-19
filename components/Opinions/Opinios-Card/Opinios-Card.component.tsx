58657154d301c45baabf8fee8f5373ddc954b426
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-19 18:35:56 +0100

    [Opinion{}Card}]] class: introduce BEM class

--- components/Opinions/Opinios-Card/Opinios-Card.component.tsx
+++ components/Opinions/Opinios-Card/Opinios-Card.component.tsx
@@ -5,22 +5,22 @@ import quotes from "@/assets/icons/quotes.png";
 
 function OpiniosCard() {
   return (
-    <section>
-      <div>
-        <div>
-          <div>
-            <Image width={30} height={30} src={quotes} alt="quotes-icon" />
+    <section className={s.container}>
+      <div className={s.container__topSideOpinion}>
+        <div className={s.container__topSideOpinion__quoteIcon}>
+          <div className={s.container__topSideOpinion__quoteIcon__wrapper}>
+            <Image className={s.container__topSideOpinion__quoteIcon__wrapper__icon} width={30} height={30} src={quotes} alt="quotes-icon" />
           </div>
         </div>
-        <p>
+        <p className={s.container__topSideOpinion__description}>
           Had a problem with my laptop's charger. Was diagnosed in 3 minutes.
           Solution done. Problem solved.
         </p>
-        <div>
+        <div className={s.container__topSideOpinion__user}>
           <span>Vadim Danilevichius</span>
         </div>
       </div>
-      <div>
+      <div className={s.container__bottomSideOpinion}>
         <span>opinión de google mapas</span>
       </div>
     </section>
