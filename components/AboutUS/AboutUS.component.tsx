c6f7445dda188cc88e49663c57f99375c43b5c17
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-19 12:29:20 +0100

    [AboutUs] class: introduce BEM class

--- components/AboutUS/AboutUS.component.tsx
+++ components/AboutUS/AboutUS.component.tsx
@@ -1,24 +1,24 @@
 import Image from "next/image";
-import React from "react";
+import s from './AboutUS.component.module.scss';
 
 function AboutUS() {
   return (
-    <section>
-      <section>
-        <div></div>
-        <div></div>
-        <Image src={""} alt="" />
+    <section className={s.container}>
+      <section className={s.container__imageSection}>
+        <div className={s.container__imageSection__frameOne}></div>
+        <div className={s.container__imageSection__frameTwo}></div>
+        <Image className={s.container__imageSection__img} src={""} alt="" />
       </section>
-      <section>
-        <div>
-          <span></span>
-          <h3></h3>
-          <p></p>
+      <section className={s.container__contentSection}>
+        <div className={s.container__contentSection__infoWrapper}>
+          <span className={s.container__contentSection__infoWrapper__category}></span>
+          <h3 className={s.container__contentSection__infoWrapper__title}></h3>
+          <p className={s.container__contentSection__infoWrapper__description}></p>
         </div>
-        <div>
-          <button>
-            <span></span>
-            <Image src={""} alt="" />
+        <div className={s.container__contentSection__buttonSection}>
+          <button className={s.container__contentSection__buttonSection__btn}>
+            <span className={s.container__contentSection__buttonSection__text}></span>
+            <Image className={s.container__contentSection__buttonSection__btn__img} src={""} alt="" />
           </button>
         </div>
       </section>
