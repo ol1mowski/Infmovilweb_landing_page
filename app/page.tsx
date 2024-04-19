fe0b31b2d58b10ba60eef4c0ae4d001727609c65
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-19 12:17:14 +0100

    [AboutUs] sketch: create a new component and introduce TSX sketch

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import AboutUS from "@/components/AboutUS/AboutUS.component";
 import Header from "@/components/Header/Header.component";
 import HeaderInfoBar from "@/components/HeaderInfoBar/HeaderInfoBar.component";
 import HomePage from "@/components/HomePage/HomePage.component";
@@ -8,6 +9,7 @@ export default function Home() {
       <HeaderInfoBar />
       <Header />
       <HomePage />
+      <AboutUS />
     </>
   );
 }
---
+++ components/AboutUS/AboutUS.component.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import React from "react";
+
+function AboutUS() {
+  return (
+    <section>
+      <section>
+        <div></div>
+        <div></div>
+        <Image src={""} alt="" />
+      </section>
+      <section>
+        <div>
+          <span></span>
+          <h3></h3>
+          <p></p>
+        </div>
+        <div>
+          <button>
+            <span></span>
+            <Image src={""} alt="" />
+          </button>
+        </div>
+      </section>
+    </section>
+  );
+}
+
+export default AboutUS;
