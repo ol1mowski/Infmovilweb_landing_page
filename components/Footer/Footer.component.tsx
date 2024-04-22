5eeb2e2a9a96a5788177985de34885a57727eacf
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-22 13:03:56 +0100

    [Footer] style: introduce full RWD to the component

--- components/Footer/Footer.component.tsx
+++ components/Footer/Footer.component.tsx
@@ -9,19 +9,24 @@ function Footer() {
         {FOOTER_ITEMS.map((i) => (
           <ItemsComponent key={i.id} items={i.items} title={i.title} />
         ))}
-        <h4 className={s.container__itemsContainer__title}>
-          INFORMACIÓN DE LA TIENDA
-        </h4>
+        <div className={s.container__itemsContainer__wrapper}>
+          <h4 className={s.container__itemsContainer__wrapper__title}>
+            INFORMACIÓN DE LA TIENDA
+          </h4>
 
-        <p className={s.container__itemsContainer__companyInfo}>
-          Infmovilweb <br /> Acorazado España nº1 Local 2 <br /> 35660 Corralejo <br /> España <br />
-          Llámenos: 928 53 56 58 <br />Envíenos un correo <br /> electrónico:
-          infmovilweb@gmail.com
-        </p>
+          <p className={s.container__itemsContainer__wrapper__companyInfo}>
+            Infmovilweb <br /> Acorazado España nº1 Local 2 <br /> 35660
+            Corralejo <br /> España <br />
+            Llámenos: 928 53 56 58 <br />
+            Envíenos un correo <br /> electrónico: infmovilweb@gmail.com
+          </p>
+        </div>
       </section>
       <section className={s.container__copyrightWrapper}>
         <span>Copyright © 2024</span>
-        <span  className={s.container__copyrightWrapper__author}>Made by Oliwier Markiewicz</span>
+        <span className={s.container__copyrightWrapper__author}>
+          Made by Oliwier Markiewicz
+        </span>
       </section>
     </footer>
   );
