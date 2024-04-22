02aebb8450a940e768c2e383b1f37b9d46a80707
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-22 11:41:44 +0100

    [Footer] component: create new component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import AboutUS from "@/components/AboutUS/AboutUS.component";
+import Footer from "@/components/Footer/Footer.component";
 import Header from "@/components/Header/Header.component";
 import HeaderInfoBar from "@/components/HeaderInfoBar/HeaderInfoBar.component";
 import HomePage from "@/components/HomePage/HomePage.component";
@@ -16,6 +17,7 @@ export default function Home() {
       <Services />
       <Opinions />
       <Location />
+      <Footer />
     </>
   );
 }
---
+++ components/Footer/Footer.component.tsx
@@ -0,0 +1,11 @@
+import s from './Footer.component.module.scss';
+
+function Footer() {
+  return (
+    <h1>
+        Footer
+    </h1>
+  )
+}
+
+export default Footer
