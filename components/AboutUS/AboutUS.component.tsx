49113d058eda8b3268b5b396c287016f69ac7f9b
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-23 13:37:03 +0100

    [utils] component: create a client side animation wrapper component

--- components/AboutUS/AboutUS.component.tsx
+++ components/AboutUS/AboutUS.component.tsx
@@ -1,37 +1,14 @@
-"use client";
-
-import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-
 import Image from "next/image";
 import s from "./AboutUS.component.module.scss";
 
 import arrow from "@/assets/icons/arrow.png";
 
 import aboutImage from "@/assets/images/about_us.jpeg";
+import AnimationWrapper from "@/utils/AnimationWrapper/AnimationWrapper.component";
 
 function AboutUS() {
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-    threshold: 0.5,
-  });
-
-  const animationVariants = {
-    hidden: { opacity: 0, y: 50 },
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: { duration: 0.5, ease: "easeOut" },
-    },
-  };
   return (
-    <motion.section
-      ref={ref}
-      initial="hidden"
-      animate={inView ? "visible" : "hidden"}
-      variants={animationVariants}
-      className={s.container}
-    >
+    <AnimationWrapper className={s.container}>
       <section className={s.container__imageSection}>
         <div className={s.container__imageSection__frameOne}></div>
         <div className={s.container__imageSection__frameTwo}></div>
@@ -72,7 +49,7 @@ function AboutUS() {
           </button>
         </div>
       </section>
-    </motion.section>
+    </AnimationWrapper>
   );
 }
 
