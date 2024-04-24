12939bbf727bf74d98043c0bd3e5747c04be037b
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-24 11:58:55 +0100

    [HamburgerMenu] sketch: add sketch to the Menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import AboutUS from "@/components/AboutUS/AboutUS.component";
 import Footer from "@/components/Footer/Footer.component";
+import HamburgerMenu from "@/components/Header/HamburgerMenu/HamburgerMenu";
 import Header from "@/components/Header/Header.component";
 import HeaderInfoBar from "@/components/HeaderInfoBar/HeaderInfoBar.component";
 import HomePage from "@/components/HomePage/HomePage.component";
@@ -12,6 +13,7 @@ export default function Home() {
     <>
       <HeaderInfoBar />
       <Header />
+      {/* <HamburgerMenu /> */}
       <HomePage />
       <AboutUS />
       <Services />
--- components/Header/HamburgerMenu/HamburgerMenu.tsx
+++ components/Header/HamburgerMenu/HamburgerMenu.tsx
@@ -1,8 +1,30 @@
+import s from "./HamburgerMenu.module.scss";
 
-import HamburgerMenuComponent from "./HamburgerMenuComponent/HamburgerMenuComponent";
+import close from "@/assets/icons/close_icon.png";
 
-const HamburgerMenu = () => {
-  return <HamburgerMenuComponent />;
+import Image from "next/image";
+
+import infmovilwebImage from '@/assets/images/infmovilweb_menuLogo.jpeg';
+
+const HamburgerMenuComponent = () => {
+  return (
+    <menu className={s.hamburgerMenu}>
+      <section className={s.hamburgerMenu__close}>
+        <Image width="30" height="30" src={close} alt="delete-sign" />
+      </section>
+      <nav className={s.hamburgerMenu__nav}>
+        <ul className={s.hamburgerMenu__nav__items}>
+          <li className={s.hamburgerMenu__nav__items__item}>Home</li>{" "}
+          <li className={s.hamburgerMenu__nav__items__item}>Sobre Nosotros</li>{" "}
+          <li className={s.hamburgerMenu__nav__items__item}>Servicios</li>{" "}
+          <li className={s.hamburgerMenu__nav__items__item}>Contacto</li>{" "}
+        </ul>
+      </nav>
+      <section>
+        <Image src={infmovilwebImage} alt="infmovilweb-image" />
+      </section>
+    </menu>
+  );
 };
 
-export default HamburgerMenu;
+export default HamburgerMenuComponent;
