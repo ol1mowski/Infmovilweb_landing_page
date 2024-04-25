96702be95465c1c99b76efa582028190363d99f8
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-25 17:14:18 +0100

    [App] ReactQueryProvider:create a ReactQueryProvider function

--- app/Root.tsx
+++ app/Root.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header/Header.component";
 import HeaderBar from "@/components/HeaderInfoBar/HeaderBar";
 import RootWrapper from "./RootWrapper";
+import ReactQueryProvider from "./ReactQueryProvider";
 
 function Root({
   children,
@@ -8,15 +9,17 @@ function Root({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pl-PL">
-      <body>
-        <RootWrapper>
-          <HeaderBar />
-          <Header />
-          {children}
-        </RootWrapper>
-      </body>
-    </html>
+    <ReactQueryProvider>
+      <html lang="pl-PL">
+        <body>
+          <RootWrapper>
+            <HeaderBar />
+            <Header />
+            {children}
+          </RootWrapper>
+        </body>
+      </html>
+    </ReactQueryProvider>
   );
 }
 
