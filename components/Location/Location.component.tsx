537a6e3a96459eb88afef26c0400f6c086950459
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-25 19:41:45 +0100

    [Footer] style: change style to companyInfo section

--- components/Location/Location.component.tsx
+++ components/Location/Location.component.tsx
@@ -1,6 +1,5 @@
 import Image, { StaticImageData } from "next/image";
 import s from "./Location.component.module.scss";
-import arrow from "@/assets/icons/arrow.png";
 import AnimationWrapper from "@/utils/AnimationWrapper/AnimationWrapper.component";
 import { fetchElements } from "@/utils/http/http";
 
@@ -12,6 +11,7 @@ async function Location() {
     category: string;
     description: string;
     title: string;
+    companyLocation: string;
   };
 
   const locationItem = fetchItems.find((item) => item.id === "location");
@@ -22,7 +22,7 @@ async function Location() {
 
   const data: DataValue = locationItem;
 
-  const { title, description, category, button } = data;
+  const { title, description, category, button, companyLocation } = data;
 
 
   return (
@@ -37,13 +37,13 @@ async function Location() {
         </p>
 
         <button className={s.container__infoSection__btn}>
-          <span>{ button.buttonText }</span> <Image src={arrow} alt="arrow-icon" />
+          <span>{ button.buttonText }</span> <Image width={25} height={25} src={button.buttonIcon} alt="arrow-icon" />
         </button>
       </section>
 
       <AnimationWrapper className={s.container__mapSection}>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3498.4149325535122!2d-13.87177592481543!3d28.737025975607832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc463502049f0711%3A0xf040117382faf600!2sINFMOVILWEB!5e0!3m2!1sen!2ses!4v1713553258330!5m2!1sen!2ses"
+          src={companyLocation}
           width="600"
           height="450"
           className={s.container__mapSection__map}
