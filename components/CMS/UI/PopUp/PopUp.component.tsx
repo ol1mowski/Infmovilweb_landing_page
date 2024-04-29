917794c2bd8fee9f88f4e6c0cbd6fb26298984d5
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-29 18:38:02 +0100

    [PopUp] refactoring: splice component tosmaller pice

--- components/CMS/UI/PopUp/PopUp.component.tsx
+++ components/CMS/UI/PopUp/PopUp.component.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import Image from "next/image";
 import s from "./PopUp.component.module.scss";
 
-import close from "@/assets/icons/close_icon.png";
 import { useContext } from "react";
 import PopUpClickContext from "@/store/PopUpClickContent";
-import Link from "next/link";
+import PopUpComponent from "./PopUpComponent/PopUpComponent.component";
 
 function PopUp() {
   let statusClass: string = "";
@@ -35,52 +33,14 @@ function PopUp() {
   const { open, action } = popUp;
 
   return (
-    <>
-      {open ? (
-        <section className={s.container}>
-          <section className={s.container__close}>
-            <div
-              className={s.container__close__wrapper}
-              onClick={closePopUpHandler}
-            >
-              <Image width={20} height={20} src={close} alt="close-icon" />
-            </div>
-          </section>
-          <section className={s.container__content}>
-            {action === "leave" ? (
-              <>
-                <h2 className={s.container__content__title}>Are You Sure ?</h2>
-                <p className={s.container__content__description}>
-                  Are you sure you want to leave this admin panel
-                </p>
-              </>
-            ) : (
-              <h2 className={statusClass}>{info}</h2>
-            )}
-          </section>
-          <section className={s.container__buttons}>
-            {action === "leave" ? (
-              <>
-                <button
-                  onClick={closePopUpHandler}
-                  className={s.container__buttons__btn_cancel}
-                >
-                  Cancel
-                </button>
-                <Link href={"/"}>
-                  <button
-                    onClick={leaveRedirectHandler}
-                    className={s.container__buttons__btn_close}
-                  >
-                    Leave
-                  </button>
-                </Link>
-              </>
-            ) : null}
-          </section>
-        </section>
-      ) : null}
-    </>
+    <PopUpComponent
+      open={open}
+      action={action}
+      statusClass={statusClass}
+      leaveRedirectHandler={leaveRedirectHandler}
+      closePopUpHandler={closePopUpHandler}
+      info={info}
+    />
   );
 }
 
