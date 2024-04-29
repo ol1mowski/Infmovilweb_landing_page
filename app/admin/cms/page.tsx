bed2ac27b756bfcfbdba904b4c68275cc1cb0a95
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-29 13:49:51 +0100

    [Location] change data feature: introduce change data feature

--- app/admin/cms/page.tsx
+++ app/admin/cms/page.tsx
@@ -3,6 +3,7 @@ import AdminBar from "@/components/CMS/AdminBar/AdminBar";
 import Header from "@/components/CMS/Header/Header.component";
 import HeaderBarFetch from "@/components/CMS/HeaderInfoBar/HeaderBar";
 import HomePage from "@/components/CMS/HomePage/HomePage.component";
+import Location from "@/components/CMS/Location/Location.component";
 import Opinions from "@/components/CMS/Opinions/Opinions.component";
 import Services from "@/components/CMS/ServicesComponent/Services.component";
 import React from "react";
@@ -17,6 +18,7 @@ function page() {
       <AboutUS />
       <Services />
       <Opinions />
+      <Location />
     </>
   );
 }
--- components/CMS/Location/LocationComponent/LocationComponent.component.tsx
+++ components/CMS/Location/LocationComponent/LocationComponent.component.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Image, { StaticImageData } from "next/image";
 import s from "./Location.component.module.scss";
 import LocationMap from "../LocationMap/LocationMap.component";
+import { useState } from "react";
 
 type LocationComponentProps = {
   category: string;
@@ -19,14 +22,40 @@ function LocationComponent({
   buttonIcon,
   buttonText,
 }: LocationComponentProps) {
+  const [categoryValue, setCategoryValue] = useState<string>(category);
+  const [titleValue, setTitleValue] = useState<string>(title);
+  const [descriptionValue, setDescriptionValue] = useState<string>(description);
+  const [buttonTextValue, setButtonTextValue] = useState<string>(buttonText);
+
   return (
     <section className={s.container}>
       <section className={s.container__infoSection}>
-        <span className={s.container__infoSection__category}>{category}</span>
-        <h3 className={s.container__infoSection__title}>{title}</h3>
-        <p className={s.container__infoSection__description}>{description}</p>
+        <input
+          type="text"
+          value={categoryValue}
+          onChange={(e) => setCategoryValue(e.target.value)}
+          className={s.container__infoSection__category}
+        />
+
+        <input
+          type="text"
+          value={titleValue}
+          onChange={(e) => setTitleValue(e.target.value)}
+          className={s.container__infoSection__title}
+        />
+        <textarea
+          value={descriptionValue}
+          wrap="true"
+          onChange={(e) => setDescriptionValue(e.target.value)}
+          className={s.container__infoSection__description}
+        />
         <button className={s.container__infoSection__btn}>
-          <span>{buttonText}</span>{" "}
+            <input
+              type="text"
+              value={buttonTextValue}
+              onChange={(e) => setButtonTextValue(e.target.value)}
+              className={s.container__infoSection__btn__text}
+            />
           <Image width={25} height={25} src={buttonIcon} alt="arrow-icon" />
         </button>
       </section>
