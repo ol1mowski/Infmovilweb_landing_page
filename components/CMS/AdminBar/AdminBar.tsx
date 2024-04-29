2fa44310be415f981e7a4b95b50eba30106bc82a
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-29 12:28:51 +0100

    [adminBar] style: add style to adminBar

--- components/CMS/AdminBar/AdminBar.tsx
+++ components/CMS/AdminBar/AdminBar.tsx
@@ -1,13 +1,30 @@
+import Link from "next/link";
 import s from "./AdminBar.module.scss";
 
 function AdminBar() {
   return (
     <section className={s.container}>
-      <div className={s.container__title}></div>
+      <div className={s.container__title}>
+        <span>Change Data Panel</span>
+      </div>
       <div className={s.container__buttonWrapper}>
-        <button className={s.container__buttonWrapper__btn}>Leave</button>
-        <button className={s.container__buttonWrapper__btn}>See Live</button>
-        <button className={s.container__buttonWrapper__btn}>Save</button>
+        <button
+          className={`${s.container__buttonWrapper__btn} ${s.container__buttonWrapper__btn_leave}`}
+        >
+          Leave
+        </button>
+        <Link href={"/"}>
+          <button
+            className={`${s.container__buttonWrapper__btn} ${s.container__buttonWrapper__btn_see}`}
+          >
+            See Live
+          </button>
+        </Link>
+        <button
+          className={`${s.container__buttonWrapper__btn} ${s.container__buttonWrapper__btn_save}`}
+        >
+          Save
+        </button>
       </div>
     </section>
   );
