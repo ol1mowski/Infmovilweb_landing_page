6e803808110158df647f83df7e99d9fe830f2b18
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-29 17:45:42 +0100

    [PopUp] functionality: introduce functionality to the popup component

--- components/CMS/UI/PopUp/PopUp.component.tsx
+++ components/CMS/UI/PopUp/PopUp.component.tsx
@@ -1,26 +1,55 @@
-import Image from 'next/image'
-import s from './PopUp.component.module.scss';
+import Image from "next/image";
+import s from "./PopUp.component.module.scss";
 
-import close from '@/assets/icons/close_icon.png';
+import close from "@/assets/icons/close_icon.png";
 
-function PopUp() {
+function PopUp({
+  popUpType,
+  saveStatus,
+}: {
+  popUpType: "leave" | "save";
+  saveStatus?: string;
+}) {
+  let statusClass: string = "";
+  let info: string = "";
+
+  if (saveStatus === "ok") {
+    statusClass = s.container__content__title_success;
+    info = "Data was success saved !";
+  } else {
+    statusClass = s.container__content__title_fail;
+    info = "Something went wront ! Please try again !";
+  }
+
   return (
-   <section className={s.container}>
-    <section className={s.container__close}>
-      <div className={s.container__close__wrapper}>
-        <Image width={20} height={20} src={close} alt='close-icon' />
-      </div>
+    <section className={s.container}>
+      <section className={s.container__close}>
+        <div className={s.container__close__wrapper}>
+          <Image width={20} height={20} src={close} alt="close-icon" />
+        </div>
+      </section>
+      <section className={s.container__content}>
+        {popUpType === "leave" ? (
+          <>
+            <h2 className={s.container__content__title}>Are You Sure ?</h2>
+            <p className={s.container__content__description}>
+              Are you sure you want to leave this admin panel
+            </p>
+          </>
+        ) : (
+          <h2 className={statusClass}>{info}</h2>
+        )}
+      </section>
+      <section className={s.container__buttons}>
+        {popUpType === "leave" ? (
+          <>
+            <button className={s.container__buttons__btn_cancel}>Cancel</button>
+            <button className={s.container__buttons__btn_close}>Leave</button>
+          </>
+        ) : null}
+      </section>
     </section>
-    <section className={s.container__content}>
-      <h2 className={s.container__content__title}>Are You Sure ?</h2>
-      <p className={s.container__content__description}>Are you sure you want to leave this admin panel</p>
-    </section>
-    <section className={s.container__buttons}>
-      <button className={s.container__buttons__btn_cancel}>Cancel</button>
-      <button className={s.container__buttons__btn_close}>Leave</button>
-    </section>
-   </section>
-  )
+  );
 }
 
-export default PopUp
+export default PopUp;
