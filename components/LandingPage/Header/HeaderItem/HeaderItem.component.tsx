8b6c3f38ecfdae2451a83ce82c18bcd69c3503cf
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-05-14 10:13:06 +0200

    [Header] links: introduce links to items section

--- components/LandingPage/Header/HeaderItem/HeaderItem.component.tsx
+++ components/LandingPage/Header/HeaderItem/HeaderItem.component.tsx
@@ -1,7 +1,10 @@
 import s from "./HeaderItem.component.module.scss";
 
-function HeaderItem({ item }: { item: string }) {
-  return <li className={s.item}>{item}</li>;
+function HeaderItem({ item }: { item: { name: string, link: string } }) {
+  
+  const { name, link } = item;
+
+  return <a href={link}><li className={s.item}>{name}</li></a>;
 }
 
 export default HeaderItem;
