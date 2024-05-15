c2e4aa724a032f4c206c0adf4b041e196249b98b
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-05-15 15:54:32 +0200

    [HomePage] refactoring and optimalize: spliting component and make code more readeable

--- components/LandingPage/HomePage/HomePage.component.tsx
+++ components/LandingPage/HomePage/HomePage.component.tsx
@@ -2,42 +2,51 @@ import s from "./HomePage.component.module.scss";
 
 import Card from "./Card-Component/Card.component";
 
-import { HomePageDataType } from "@/utils/DataTypes/DataTypes";
+import { type HomePageDataType } from "@/utils/DataTypes/DataTypes";
 
 async function HomePage() {
-  const fetchItems = await fetch("http://127.0.0.1:8080/api/homepage", {
-    cache: "no-store",
-  });
+  try {
+    const fetchItems = await fetch("http://127.0.0.1:8080/api/homepage", {
+      cache: "no-store",
+    });
 
-  const fetchedItems: HomePageDataType = await fetchItems.json();
+    if (!fetchItems.ok) {
+      throw new Error(`Fetching failed with status: ${fetchItems.status}`);
+    }
 
-  if (!fetchedItems) {
-    throw new Error("Fetching failed...");
+    const fetchedItems: HomePageDataType = await fetchItems.json();
+
+    if (!fetchedItems) {
+      throw new Error("Fetching failed...");
+    }
+
+    const { Cards } = fetchedItems[0];
+
+    if (!Cards) {
+      throw new Error("Faltan algunas propiedades requeridas.");
+    }
+
+    return (
+      <section id="home" className={s.container}>
+        <div className={s.container__cardsWrapper}>
+          {Cards.map((c) => (
+            <Card
+              buttonIcon={c.button.buttonIcon}
+              buttonText={c.button.buttonText}
+              key={c.title}
+              logo={c.icon}
+              link={c.link}
+              description={c.description}
+              title={c.title}
+            />
+          ))}
+        </div>
+      </section>
+    );
+  } catch (error) {
+    console.error("Error fetching header bar data:", error);
+    throw error;
   }
-
-  const { Cards } = fetchedItems[0];
-
-  if (!Cards) {
-    throw new Error("Faltan algunas propiedades requeridas.");
-  }
-
-  return (
-    <section id="home" className={s.container}>
-      <div className={s.container__cardsWrapper}>
-        {Cards.map((c) => (
-          <Card
-            buttonIcon={c.button.buttonIcon}
-            buttonText={c.button.buttonText}
-            key={c.title}
-            logo={c.icon}
-            link={c.link}
-            description={c.description}
-            title={c.title}
-          />
-        ))}
-      </div>
-    </section>
-  );
 }
 
 export default HomePage;
